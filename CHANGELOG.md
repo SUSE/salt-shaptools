9203b3adf6c18627d213ce885c184cf2212a4966
Xabier Arbulu Insausti <xarbulu@suse.com>
2018-12-04 11:45:32 +0000

    Add travis ci file

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,3 @@
-# Version 0.2.0
-- SAP HANA installation features added
-- Move modules and states folder to salt main folder
-
 # Version 0.1.0
+- SAP HANA installation features added
 - SAP HANA system replication initial features added
--- tests/run.sh
+++ tests/run.sh
@@ -2,5 +2,4 @@ cp salt/modules/*.py ../salt/salt/modules/
 cp salt/states/*.py ../salt/salt/states/
 cp tests/unit/modules/*.py ../salt/tests/unit/modules/
 cp tests/unit/states/*.py ../salt/tests/unit/states/
-../salt/tests/runtests.py -v -n unit.modules.test_hanamod
-../salt/tests/runtests.py -v -n unit.states.test_hanamod
+py.test ../salt/tests/unit/modules/test_hanamod.py ../salt/tests/unit/states/test_hanamod.py -vv --cov=salt.modules.hanamod --cov=salt.states.hanamod --cov-config .coveragerc --cov-report term
