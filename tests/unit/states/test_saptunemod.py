d8a85c3005868cc908ab11d633555c3ae6b3c610
dmaiocchi <dmaiocchi@suse.com>
2020-01-09 12:11:31 +0100

    Add tests for saptunemod state

---
+++ tests/unit/states/test_saptunemod.py
@@ -0,0 +1,49 @@
+
+# -*- coding: utf-8 -*-
+'''
+    :codeauthor: Dario Maiocchi <dmaiocchi@suse.com>
+'''
+# Import Python libs
+from __future__ import absolute_import, unicode_literals, print_function
+
+from salt import exceptions
+
+# Import Salt Testing Libs
+from tests.support.mixins import LoaderModuleMockMixin
+from tests.support.unit import skipIf, TestCase
+from tests.support import mock
+from tests.support.mock import (
+    NO_MOCK,
+    NO_MOCK_REASON,
+    MagicMock,
+    patch
+)
+
+# Import Salt Libs
+import salt.states.saptunemod as saptune
+
+
+@skipIf(NO_MOCK, NO_MOCK_REASON)
+class SaptunemodTestCase(TestCase, LoaderModuleMockMixin):
+    '''
+    Test cases for salt.states.saptunemod
+    '''
+    def setup_loader_modules(self):
+        return {saptune: {'__opts__': {'test': False}}}
+
+
+    def test_solution_applied(self):
+        '''
+        Test solution is applied method
+        '''
+
+        ret = {'name': 'fakesolution',
+               'changes': {},
+               'result': True,
+               'comment': 'Saptune solution applied'}
+
+        reponse = MagicMock(return_value=True)
+        with patch.dict(saptune.__salt__, {'saptune.apply_solution': reponse}):
+            assert saptune.apply_solution(
+                'fakesolution', {}, True, 'Saptune solution applied') == ret
+
